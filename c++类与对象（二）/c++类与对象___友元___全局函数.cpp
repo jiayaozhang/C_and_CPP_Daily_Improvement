1620a440bedc27ce22a1b24b09b508922c4c79c6
Jiayao Zhang 张嘉瑶 <jiayao_zhang@outlook.com>
2021-07-08 12:32:54 +0300

    add the second section

---
+++ c++类与对象（二）/c++类与对象___友元___全局函数.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+using namespace std;
+#include<string>
+
+//友元的目的是让一些私有属性也可以被类外特殊的函数访问到
+//友元的关键字为 friend
+//友元有三种实现方式
+//1.全局函数做友元
+//2.类做友元
+//3.成员函数做友元
+
+
+class Building
+{
+	//goodGay全局函数是building好朋友，可以访问building中的私有成员
+	friend void goodGay(Building* building);
+
+
+public:
+	Building()
+	{
+		this->m_BedRoom = "卧室";
+		this->m_SittingRoom = "客厅";
+	}
+
+public:
+	string m_SittingRoom;
+
+private:
+	string m_BedRoom;
+};
+
+void goodGay(Building* building)
+{
+	cout << "好基友的全局函数正在访问：" << building->m_SittingRoom << endl;
+	cout << "好基友的全局函数正在访问：" << building->m_BedRoom << endl;
+}
+
+void test01()
+{
+	Building building;
+	goodGay(&building);
+}
+
+int main()
+{
+	test01();
+	system("pause");
+	return 0;
+}
---
+++ c++类与对象（二）/c++类与对象___对象特性___const修饰成员函数.cpp
@@ -0,0 +1,134 @@
+﻿#include <iostream>
+using namespace std;
+#include<string>
+
+//常函数
+//成员函数后加const后 称为常函数
+//常函数不可以修改成员属性
+//成员属性声明时加关键字mutable后，在常函数中依旧可以修改
+
+//常对象
+//声明对象前加const称该对象为常对象
+//常对象只能调用常函数
+
+class Person
+{
+public:
+	//this指针的本质是指针常量，指针的指向是不可以修改的
+	//Person *const this;
+	//在成员函数后加const，修饰的是this指向，让指针指向的值不可以修改
+	void showPerson() const
+	{
+		this->m_B = 100;
+		// this = NULL ; //this指针不可以修改指针的指向的
+	}
+
+	void func()
+	{
+		m_A = 100;
+	}
+
+	int m_A;
+	mutable int m_B; //特殊变量，即使在常函数中，也可以修改这个值
+};
+
+
+//解决名称冲突
+void test01()
+{
+	Person p;
+	p.showPerson();
+}
+
+
+
+//返回对象本身用 *this
+
+void test02()
+{
+	const Person p;
+	p.m_B = 100; //m_B是特殊变量，在常对象下也可以修改
+
+	//常对象只能调用常函数
+	p.showPerson();
+	//p.func();  常对象 不可以调用普通成员函数
+
+}
+
+
+
+int main()
+{
+	test01();
+
+	test02();
+	system("pause");
+	return 0;
+}
+#include <iostream>
+using namespace std;
+#include<string>
+
+//常函数
+//成员函数后加const后 称为常函数
+//常函数不可以修改成员属性
+//成员属性声明时加关键字mutable后，在常函数中依旧可以修改
+
+//常对象
+//声明对象前加const称该对象为常对象
+//常对象只能调用常函数
+
+class Person
+{
+public:
+	//this指针的本质是指针常量，指针的指向是不可以修改的
+	//Person *const this;
+	//在成员函数后加const，修饰的是this指向，让指针指向的值不可以修改
+	void showPerson() const
+	{
+		this->m_B = 100;
+		// this = NULL ; //this指针不可以修改指针的指向的
+	}
+
+	void func()
+	{
+		m_A = 100;
+	}
+
+	int m_A;
+	mutable int m_B; //特殊变量，即使在常函数中，也可以修改这个值
+};
+
+
+//解决名称冲突
+void test01()
+{
+	Person p;
+	p.showPerson();
+}
+
+
+
+//返回对象本身用 *this
+
+void test02()
+{
+	const Person p;
+	p.m_B = 100; //m_B是特殊变量，在常对象下也可以修改
+
+	//常对象只能调用常函数
+	p.showPerson();
+	//p.func();  常对象 不可以调用普通成员函数
+
+}
+
+
+
+int main()
+{
+	test01();
+
+	test02();
+	system("pause");
+	return 0;
+}
