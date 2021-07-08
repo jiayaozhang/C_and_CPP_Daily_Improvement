45b57a725007ee6f966228404257956c354ce42e
Jiayao Zhang 张嘉瑶 <jiayao_zhang@outlook.com>
2021-07-08 16:21:30 +0300

    Add files via upload

---
+++ c++类与对象（二）/c++类与对象___c++运算符重载___递增运算符重载.cpp
@@ -0,0 +1,66 @@
+﻿#include <iostream>
+using namespace std;
+#include<string>
+
+//左移运算符重载
+
+class MyInteger
+{
+	friend ostream& operator << (ostream& out, MyInteger myint);
+public:
+	MyInteger()
+	{
+		m_Num = 0;
+	}
+	//重载前置++ 运算符
+	MyInteger& operator++()
+	{
+		m_Num++;
+		return *this;
+	}
+
+	//重载后置++ 运算符
+	MyInteger operator++(int)
+	{
+		//先 记录结果
+		MyInteger temp = *this;
+		//后 递增
+		m_Num++;
+		//最后将记录结果做返回
+		return temp;
+	}
+
+private:
+
+	int m_Num;
+};
+
+ostream& operator << (ostream& out, MyInteger myint)
+{
+	cout << myint.m_Num;
+	return cout;
+}
+
+void test01()
+{
+	MyInteger myint;
+	cout << ++(++myint) << endl;
+	cout << myint << endl;
+}
+
+void test02()
+{
+	MyInteger myint;
+	cout << myint++ << endl;
+	cout << (myint++)++ << endl;
+}
+
+int main()
+{
+	test01();
+	test02();
+	system("pause");
+	return 0;
+}
+
+//左移运算符配合友元可以实现输出自定义数据类型
