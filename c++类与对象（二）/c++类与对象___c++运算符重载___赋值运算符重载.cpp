b068db912c7a1b63e274acef6b30b4dde4efa49b
Jiayao Zhang 张嘉瑶 <jiayao_zhang@outlook.com>
2021-07-08 16:53:54 +0300

    Add files via upload

---
+++ c++类与对象（二）/c++类与对象___c++运算符重载___赋值运算符重载.cpp
@@ -0,0 +1,66 @@
+﻿#include <iostream>
+using namespace std;
+#include<string>
+
+//左移运算符重载
+
+class Person
+{
+public:
+
+	Person(int age)
+	{
+		m_Age = new int(age);
+	}
+
+	~Person()
+	{
+		if (m_Age != NULL)
+		{
+			delete m_Age;
+			m_Age = NULL;
+		}
+	}
+
+	//赋值运算符重载
+	Person& operator=(Person& p)
+	{
+		if (m_Age != NULL)
+		{
+			delete m_Age;
+			m_Age = NULL;
+		}
+		//深拷贝
+		m_Age = new int(*p.m_Age);
+
+		return *this;
+	}
+
+	int* m_Age;
+};
+
+void test01()
+{
+	Person p1(18);
+	Person p2(20);
+	Person p3(30);
+
+	p3 = p2 = p1; //赋值运算
+
+	cout << "p1的年龄为" << *p1.m_Age << endl;
+
+	cout << "p2的年龄为" << *p2.m_Age << endl;
+
+	cout << "p3的年龄为" << *p3.m_Age << endl;
+
+}
+
+
+int main()
+{
+	test01();
+	system("pause");
+	return 0;
+}
+
+//左移运算符配合友元可以实现输出自定义数据类型
