8c694897e6f1feba99ad8f77ddcc01095579039a
Jiayao Zhang 张嘉瑶 <jiayao_zhang@outlook.com>
2021-07-09 10:13:59 +0300

    Add files via upload

---
+++ c++类与对象（二）/c++类与对象___继承___继承中的对象模型.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+using namespace std;
+#include<string>
+
+//公共页面类
+class BasePage
+{
+public:
+	int m_A;
+protected:
+	int m_B;
+private:
+	int m_C;
+};
+
+class Son : public BasePage
+{
+public:
+	int m_D;
+
+};
+
+void test01()
+{
+	cout << "size of son = " << sizeof(Son) << endl;
+}
+
+
+int main()
+{
+	test01();
+	system("pause");
+	return 0;
+}
+
