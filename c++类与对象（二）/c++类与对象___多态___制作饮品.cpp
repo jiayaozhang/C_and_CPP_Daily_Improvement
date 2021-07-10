e9eab73e9074dbb893d2c49dd5d310d6fa9074f5
Jiayao Zhang 张嘉瑶 <jiayao_zhang@outlook.com>
2021-07-10 14:34:21 +0300

    Add files via upload

---
+++ c++类与对象（二）/c++类与对象___多态___制作饮品.cpp
@@ -0,0 +1,103 @@
+﻿#include <iostream>
+using namespace std;
+#include<string>
+
+class AbstractDrinking
+{
+public:
+	//煮水
+	virtual void Boil() = 0;
+
+	//冲泡
+	virtual void Brew() = 0;
+
+	//倒入水中
+	virtual void PourInCup() = 0;
+
+	//加入辅料
+	virtual void PutSomething() = 0;
+
+	//制作饮品
+	void makeDrink()
+	{
+		Boil();
+		Brew();
+		PourInCup();
+		PutSomething();
+	}
+};
+
+class coffee : public AbstractDrinking
+{
+public:
+	virtual void Boil()
+	{
+		cout << "煮农夫山泉" << endl;
+	}
+
+	virtual void Brew()
+	{
+		cout << "冲泡咖啡" << endl;
+	}
+
+	virtual void PourInCup()
+	{
+		cout << "倒入水中" << endl;
+	}
+
+	virtual void PutSomething()
+	{
+		cout << "加入糖和牛奶" << endl;
+	}
+};
+
+class Tea : public AbstractDrinking
+{
+public:
+	virtual void Boil()
+	{
+		cout << "煮农夫山泉" << endl;
+	}
+
+	virtual void Brew()
+	{
+		cout << "冲泡咖啡" << endl;
+	}
+
+	virtual void PourInCup()
+	{
+		cout << "倒入水中" << endl;
+	}
+
+	virtual void PutSomething()
+	{
+		cout << "加入糖和牛奶" << endl;
+	}
+};
+
+
+void doWork(AbstractDrinking* abs)
+{
+	abs->makeDrink();
+	delete abs; //释放
+}
+
+
+void test01()
+{
+	//制作咖啡
+	doWork(new coffee);
+
+
+	cout << "-------------------" << endl;
+	doWork(new Tea);
+
+}
+
+int main()
+{
+	test01();
+	system("pause");
+	return 0;
+}
+
