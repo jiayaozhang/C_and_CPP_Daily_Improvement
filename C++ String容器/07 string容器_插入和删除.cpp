c71fc78df5a8dbab03ee05852ce9c54d050df4f4
Jiayao Zhang 张嘉瑶 <jiayao_zhang@outlook.com>
2021-08-09 13:48:25 +0300

    Update 07 string容器_插入和删除.cpp

--- C++ String容器/07 string容器_插入和删除.cpp
+++ C++ String容器/07 string容器_插入和删除.cpp
@@ -1,27 +1,27 @@
-//#include<iostream>
-//using namespace std;
-//#include <string>
-//
-////×Ö·û´® ²åÈëºÍÉ¾³ı
-//void test01()
-//{
-//	string str = "hello";
-//
-//	//²åÈë
-//	str.insert(1, "111");
-//	//h111ello
-//	cout << "str = " << str << endl;
-//
-//	//É¾³ı
-//	str.erase(1, 3);
-//	cout << "str = " << str << endl;
-//}
-//
-//int main() {
-//
-//	test01();
-//
-//	system("pause");
-//
-//	return 0;
-//}
+#include<iostream>
+using namespace std;
+#include <string>
+
+//å­—ç¬¦ä¸² æ’å…¥å’Œåˆ é™¤
+void test01()
+{
+	string str = "hello";
+
+	//æ’å…¥
+	str.insert(1, "111");
+	//h111ello
+	cout << "str = " << str << endl;
+
+	//åˆ é™¤
+	str.erase(1, 3);
+	cout << "str = " << str << endl;
+}
+
+int main() {
+
+	test01();
+
+	system("pause");
+
+	return 0;
+}
