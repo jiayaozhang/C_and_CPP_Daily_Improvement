d8c9f19411424dcdcc8d908fffecc562e6aa19af
Jiayao Zhang 张嘉瑶 <jiayao_zhang@outlook.com>
2021-08-11 15:39:19 +0300

    Update 01 deque容器_构造函数.cpp

--- C++ deque容器/01 deque容器_构造函数.cpp
+++ C++ deque容器/01 deque容器_构造函数.cpp
@@ -1,42 +1,42 @@
-//#include<iostream>
-//using namespace std;
-//#include <deque>
-////deque ¹¹Ôìº¯Êı
-//
-//void printDeque(const deque<int>&d)
-//{
-//	for (deque<int>::const_iterator it = d.begin(); it != d.end(); it++)
-//	{
-//		//*it = 100;  ÈİÆ÷ÖĞµÄÊı¾İ²»¿ÉÒÔĞŞ¸ÄÁË
-//		cout << *it << " ";
-//	}
-//	cout << endl;
-//}
-//
-//void test01()
-//{
-//	deque<int>d1;
-//	for (int i = 0; i < 10; i++)
-//	{
-//		d1.push_back(i);
-//	}
-//	printDeque(d1);
-//
-//	deque<int>d2(d1.begin(), d1.end());
-//	printDeque(d2);
-//
-//	deque<int>d3(10, 100);
-//	printDeque(d3);
-//
-//	deque<int>d4(d3);
-//	printDeque(d4);
-//}
-//
-//int main() {
-//
-//	test01();
-//
-//	system("pause");
-//
-//	return 0;
-//}
+#include<iostream>
+using namespace std;
+#include <deque>
+//deque æ„é€ å‡½æ•°
+
+void printDeque(const deque<int>&d)
+{
+	for (deque<int>::const_iterator it = d.begin(); it != d.end(); it++)
+	{
+		//*it = 100;  å®¹å™¨ä¸­çš„æ•°æ®ä¸å¯ä»¥ä¿®æ”¹äº†
+		cout << *it << " ";
+	}
+	cout << endl;
+}
+
+void test01()
+{
+	deque<int>d1;
+	for (int i = 0; i < 10; i++)
+	{
+		d1.push_back(i);
+	}
+	printDeque(d1);
+
+	deque<int>d2(d1.begin(), d1.end());
+	printDeque(d2);
+
+	deque<int>d3(10, 100);
+	printDeque(d3);
+
+	deque<int>d4(d3);
+	printDeque(d4);
+}
+
+int main() {
+
+	test01();
+
+	system("pause");
+
+	return 0;
+}
