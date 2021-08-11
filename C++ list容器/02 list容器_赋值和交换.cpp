8b785024f30ba3bf4b1562663f9f65b65fece8e5
Jiayao Zhang 张嘉瑶 <jiayao_zhang@outlook.com>
2021-08-11 17:58:43 +0300

    Update 02 list容器_赋值和交换.cpp

--- C++ list容器/02 list容器_赋值和交换.cpp
+++ C++ list容器/02 list容器_赋值和交换.cpp
@@ -1,73 +1,73 @@
-//#include<iostream>
-//using namespace std;
-//#include <list>
-//
-////listÈİÆ÷¸³ÖµºÍ½»»»
-//void printList(const list<int>&L)
-//{
-//	for (list<int>::const_iterator it = L.begin(); it != L.end(); it++)
-//	{
-//		cout << *it << " ";
-//	}
-//	cout << endl;
-//}
-//
-////¸³Öµ
-//void test01()
-//{
-//	list<int>L1;
-//
-//	L1.push_back(10);
-//	L1.push_back(20);
-//	L1.push_back(30);
-//	L1.push_back(40);
-//
-//	printList(L1);
-//
-//	list<int>L2;
-//	L2 = L1; // operator= ¸³Öµ
-//	printList(L2);
-//
-//
-//	list<int>L3;
-//	L3.assign(L2.begin(), L2.end());
-//	printList(L3);
-//
-//	list<int>L4;
-//	L4.assign(10, 100);
-//	printList(L4);
-//
-//}
-//
-////½»»»
-//void test02()
-//{
-//	list<int>L1;
-//
-//	L1.push_back(10);
-//	L1.push_back(20);
-//	L1.push_back(30);
-//	L1.push_back(40);
-//
-//	list<int>L2;
-//	L2.assign(10, 100);
-//
-//	cout << "½»»»Ç°£º " << endl;
-//	printList(L1);
-//	printList(L2);
-//
-//	L1.swap(L2);
-//	cout << "½»»»ºó£º " << endl;
-//	printList(L1);
-//	printList(L2);
-//}
-//
-//int main() {
-//
-//	//test01();
-//	test02();
-//
-//	system("pause");
-//
-//	return 0;
-//}
+#include<iostream>
+using namespace std;
+#include <list>
+
+//listå®¹å™¨èµ‹å€¼å’Œäº¤æ¢
+void printList(const list<int>&L)
+{
+	for (list<int>::const_iterator it = L.begin(); it != L.end(); it++)
+	{
+		cout << *it << " ";
+	}
+	cout << endl;
+}
+
+//èµ‹å€¼
+void test01()
+{
+	list<int>L1;
+
+	L1.push_back(10);
+	L1.push_back(20);
+	L1.push_back(30);
+	L1.push_back(40);
+
+	printList(L1);
+
+	list<int>L2;
+	L2 = L1; // operator= èµ‹å€¼
+	printList(L2);
+
+
+	list<int>L3;
+	L3.assign(L2.begin(), L2.end());
+	printList(L3);
+
+	list<int>L4;
+	L4.assign(10, 100);
+	printList(L4);
+
+}
+
+//äº¤æ¢
+void test02()
+{
+	list<int>L1;
+
+	L1.push_back(10);
+	L1.push_back(20);
+	L1.push_back(30);
+	L1.push_back(40);
+
+	list<int>L2;
+	L2.assign(10, 100);
+
+	cout << "äº¤æ¢å‰ï¼š " << endl;
+	printList(L1);
+	printList(L2);
+
+	L1.swap(L2);
+	cout << "äº¤æ¢åï¼š " << endl;
+	printList(L1);
+	printList(L2);
+}
+
+int main() {
+
+	//test01();
+	test02();
+
+	system("pause");
+
+	return 0;
+}
