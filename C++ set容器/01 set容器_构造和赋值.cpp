6ce5c9e3fc30297222af75b9f8bd4467004c748f
Jiayao Zhang 张嘉瑶 <jiayao_zhang@outlook.com>
2021-08-18 11:51:47 +0300

    Update 01 set容器_构造和赋值.cpp

--- C++ set容器/01 set容器_构造和赋值.cpp
+++ C++ set容器/01 set容器_构造和赋值.cpp
@@ -1,50 +1,50 @@
-//#include<iostream>
-//using namespace std;
-//#include <set>
-//
-////setÈİÆ÷¹¹ÔìºÍ¸³Öµ
-//void printSet(set<int>&s)
-//{
-//	for (set<int>::iterator it = s.begin(); it != s.end(); it++)
-//	{
-//		cout << *it << " ";
-//	}
-//	cout << endl;
-//}
-//
-//void test01()
-//{
-//	set<int>s1;
-//
-//	//²åÈëÊı¾İ Ö»ÓĞinsert·½Ê½
-//	s1.insert(10);
-//	s1.insert(40);
-//	s1.insert(30);
-//	s1.insert(20);
-//	s1.insert(30);
-//
-//
-//	//±éÀúÈİÆ÷
-//	//setÈİÆ÷ÌØµã£ºËùÓĞÔªËØ²åÈëÊ±ºò×Ô¶¯±»ÅÅĞò
-//	//setÈİÆ÷²»ÔÊĞí²åÈëÖØ¸´Öµ
-//	printSet(s1);
-//
-//	//¿½±´¹¹Ôì
-//	set<int>s2(s1);
-//	printSet(s2);
-//
-//
-//	//¸³Öµ
-//	set<int>s3;
-//	s3 = s2;
-//	printSet(s3);
-//}
-//
-//int main() {
-//
-//	test01();
-//
-//	system("pause");
-//
-//	return 0;
-//}
+#include<iostream>
+using namespace std;
+#include <set>
+
+//setå®¹å™¨æ„é€ å’Œèµ‹å€¼
+void printSet(set<int>&s)
+{
+	for (set<int>::iterator it = s.begin(); it != s.end(); it++)
+	{
+		cout << *it << " ";
+	}
+	cout << endl;
+}
+
+void test01()
+{
+	set<int>s1;
+
+	//æ’å…¥æ•°æ® åªæœ‰insertæ–¹å¼
+	s1.insert(10);
+	s1.insert(40);
+	s1.insert(30);
+	s1.insert(20);
+	s1.insert(30);
+
+
+	//éå†å®¹å™¨
+	//setå®¹å™¨ç‰¹ç‚¹ï¼šæ‰€æœ‰å…ƒç´ æ’å…¥æ—¶å€™è‡ªåŠ¨è¢«æ’åº
+	//setå®¹å™¨ä¸å…è®¸æ’å…¥é‡å¤å€¼
+	printSet(s1);
+
+	//æ‹·è´æ„é€ 
+	set<int>s2(s1);
+	printSet(s2);
+
+
+	//èµ‹å€¼
+	set<int>s3;
+	s3 = s2;
+	printSet(s3);
+}
+
+int main() {
+
+	test01();
+
+	system("pause");
+
+	return 0;
+}
