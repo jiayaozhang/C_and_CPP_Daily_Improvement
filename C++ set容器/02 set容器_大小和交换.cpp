0b6569ca7330276c7c6e81a55cbff574e6c42e6e
Jiayao Zhang 张嘉瑶 <jiayao_zhang@outlook.com>
2021-08-18 12:01:53 +0300

    Update 02 set容器_大小和交换.cpp

--- C++ set容器/02 set容器_大小和交换.cpp
+++ C++ set容器/02 set容器_大小和交换.cpp
@@ -1,80 +1,80 @@
-//#include<iostream>
-//using namespace std;
-//#include <set>
-//
-////setÈİÆ÷ ´óĞ¡ºÍ½»»»
-//
-//void printSet(set<int>&s)
-//{
-//	for (set<int>::iterator it = s.begin(); it != s.end(); it++)
-//	{
-//		cout << *it << " ";
-//	}
-//	cout << endl;
-//}
-//
-////´óĞ¡
-//void test01()
-//{
-//	set<int>s1;
-//
-//	//²åÈëÊı¾İ
-//	s1.insert(10);
-//	s1.insert(30);
-//	s1.insert(20);
-//	s1.insert(40);
-//
-//	//´òÓ¡ÈİÆ÷
-//	printSet(s1);
-//
-//	//ÅĞ¶ÏÊÇ·ñÎª¿Õ
-//	if (s1.empty())
-//	{
-//		cout << "s1Îª¿Õ" << endl;
-//	}
-//	else
-//	{
-//		cout << "s1²»Îª¿Õ" << endl;
-//		cout << "s1µÄ´óĞ¡Îª£º " << s1.size() << endl;
-//	}
-//}
-//
-////½»»»
-//void test02()
-//{
-//	set<int>s1;
-//
-//	//²åÈëÊı¾İ
-//	s1.insert(10);
-//	s1.insert(30);
-//	s1.insert(20);
-//	s1.insert(40);
-//
-//	set<int>s2;
-//
-//	//²åÈëÊı¾İ
-//	s2.insert(100);
-//	s2.insert(300);
-//	s2.insert(200);
-//	s2.insert(400);
-//
-//	cout << "½»»»Ç°£º " << endl;
-//	printSet(s1);
-//	printSet(s2);
-//
-//	cout << "½»»»ºó£º " << endl;
-//	s1.swap(s2);
-//	printSet(s1);
-//	printSet(s2);
-//}
-//
-//int main() {
-//
-//	//test01();
-//
-//	test02();
-//
-//	system("pause");
-//
-//	return 0;
-//}
+#include<iostream>
+using namespace std;
+#include <set>
+
+//setå®¹å™¨ å¤§å°å’Œäº¤æ¢
+
+void printSet(set<int>&s)
+{
+	for (set<int>::iterator it = s.begin(); it != s.end(); it++)
+	{
+		cout << *it << " ";
+	}
+	cout << endl;
+}
+
+//å¤§å°
+void test01()
+{
+	set<int>s1;
+
+	//æ’å…¥æ•°æ®
+	s1.insert(10);
+	s1.insert(30);
+	s1.insert(20);
+	s1.insert(40);
+
+	//æ‰“å°å®¹å™¨
+	printSet(s1);
+
+	//åˆ¤æ–­æ˜¯å¦ä¸ºç©º
+	if (s1.empty())
+	{
+		cout << "s1ä¸ºç©º" << endl;
+	}
+	else
+	{
+		cout << "s1ä¸ä¸ºç©º" << endl;
+		cout << "s1çš„å¤§å°ä¸ºï¼š " << s1.size() << endl;
+	}
+}
+
+//äº¤æ¢
+void test02()
+{
+	set<int>s1;
+
+	//æ’å…¥æ•°æ®
+	s1.insert(10);
+	s1.insert(30);
+	s1.insert(20);
+	s1.insert(40);
+
+	set<int>s2;
+
+	//æ’å…¥æ•°æ®
+	s2.insert(100);
+	s2.insert(300);
+	s2.insert(200);
+	s2.insert(400);
+
+	cout << "äº¤æ¢å‰ï¼š " << endl;
+	printSet(s1);
+	printSet(s2);
+
+	cout << "äº¤æ¢åï¼š " << endl;
+	s1.swap(s2);
+	printSet(s1);
+	printSet(s2);
+}
+
+int main() {
+
+	//test01();
+
+	test02();
+
+	system("pause");
+
+	return 0;
+}
