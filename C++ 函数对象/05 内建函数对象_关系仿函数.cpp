bd2e95d8968d33a8eae6a7b06dbadd378269887e
Jiayao Zhang 张嘉瑶 <jiayao_zhang@outlook.com>
2021-08-22 14:12:05 +0300

    Update 05 内建函数对象_关系仿函数.cpp

--- C++ 函数对象/05 内建函数对象_关系仿函数.cpp
+++ C++ 函数对象/05 内建函数对象_关系仿函数.cpp
@@ -1,54 +1,54 @@
-//#include<iostream>
-//using namespace std;
-//#include <vector>
-//#include <algorithm>
-//#include <functional>
-//
-//
-////ÄÚ½¨º¯Êý¶ÔÏó _¹ØÏµ·Âº¯Êý  
-////´óÓÚ greater
-//class MyCompare 
-//{
-//public:
-//	bool operator()(int v1,int v2)
-//	{
-//		return v1 > v2;
-//	}
-//};
-//
-//void test01()
-//{
-//	vector<int>v;
-//
-//	v.push_back(10);
-//	v.push_back(30);
-//	v.push_back(40);
-//	v.push_back(20);
-//	v.push_back(50);
-//
-//	for (vector<int>::iterator it = v.begin(); it != v.end(); it++)
-//	{
-//		cout << *it << " ";
-//	}
-//	cout << endl;
-//
-//	//½µÐò
-//	//sort(v.begin(), v.end(), MyCompare());
-//	//greater<int>() ÄÚ½¨º¯Êý¶ÔÏó
-//	sort(v.begin(), v.end(), greater<int>());
-//	for (vector<int>::iterator it = v.begin(); it != v.end(); it++)
-//	{
-//		cout << *it << " ";
-//	}
-//	cout << endl;
-//
-//}
-//
-//int main() {
-//
-//	test01();
-//
-//	system("pause");
-//
-//	return 0;
-//}
+#include<iostream>
+using namespace std;
+#include <vector>
+#include <algorithm>
+#include <functional>
+
+
+//å†…å»ºå‡½æ•°å¯¹è±¡ _å…³ç³»ä»¿å‡½æ•°  
+//å¤§äºŽ greater
+class MyCompare 
+{
+public:
+	bool operator()(int v1,int v2)
+	{
+		return v1 > v2;
+	}
+};
+
+void test01()
+{
+	vector<int>v;
+
+	v.push_back(10);
+	v.push_back(30);
+	v.push_back(40);
+	v.push_back(20);
+	v.push_back(50);
+
+	for (vector<int>::iterator it = v.begin(); it != v.end(); it++)
+	{
+		cout << *it << " ";
+	}
+	cout << endl;
+
+	//é™åº
+	//sort(v.begin(), v.end(), MyCompare());
+	//greater<int>() å†…å»ºå‡½æ•°å¯¹è±¡
+	sort(v.begin(), v.end(), greater<int>());
+	for (vector<int>::iterator it = v.begin(); it != v.end(); it++)
+	{
+		cout << *it << " ";
+	}
+	cout << endl;
+
+}
+
+int main() {
+
+	test01();
+
+	system("pause");
+
+	return 0;
+}
