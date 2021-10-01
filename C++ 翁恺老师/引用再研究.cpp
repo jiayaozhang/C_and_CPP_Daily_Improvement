a3d2ceae1b979b87816aebcb8eb0dad81fd61765
Jiayao Zhang 张嘉瑶 <jiayao_zhang@outlook.com>
2021-10-01 16:28:33 +0800

    Update 引用再研究.cpp

--- C++ 翁恺老师/引用再研究.cpp
+++ C++ 翁恺老师/引用再研究.cpp
@@ -1,25 +1,12 @@
-#include "stdafx.h"
-#include <memory>
+declared without initial value
 
-// 为什么有强弱指针
-// A{
-//   B对象智能指针 （引用次数 1）  需要lock函数提升 
-// weak_ptr_uses_count
-// }
-// B{
-//  A 对象智能指针 （引用次数 2）  
-// shared_ptr_uses_count
-//}
+must be initialized using constructor initializer list
 
-int _tmain( int argc, _TCHAR* argv[])
-{
-    std::shared_ptr<int> sptr(new int(3));
-    std::shared_ptr<int> sptr2 = sptr2;
-    std::weak_ptr<int> wptr = sptr;
+class X{
+public:
 
-    if(!wptr.expired()){
-        std::shared_ptr<int> sptr3 = wptr.lock();
-    }
+        int& m_y;
+        X(int& a);
+       };
 
-    return 0;
-}
+X::X(int& a) : m_y(a) {}
