7efe421ed761caf24f0d7b96bbbc1e0ad1fd8646
jiayaozhang <jiayao.zhang@kaust.edu.sa>
2023-05-20 16:35:25 +0800

    macros

---
+++ TheChernoCppTutorial/47-MacrosInCpp/run.sh
@@ -0,0 +1,15 @@
+g++ 47main.cpp -o 47main
+./47main
+
+
+g++ 47main1.cpp -o 47main1
+./47main1
+
+g++ 47main2.cpp -o 47main2
+./47main2
+
+g++ 47main3.cpp -o 47main3
+./47main3
+
+g++ 47main4.cpp -o 47main4
+./47main4
